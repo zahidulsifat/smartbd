69cf1b62ea40f0a3692307648e83a9319f589f55
Zahidul Sifat <121428209+zahidulsifat@users.noreply.github.com>
2025-03-01 02:54:13 +0600

    Update form handelar.php

--- form handelar.php
+++ form handelar.php
@@ -13,7 +13,7 @@ $email_body = "User Name: $name.\n".
                  "Subject: $subject.\n".
                    "User Message: $message .\n";
 
-$to = 'rafiasaima496@gmail.com';
+$to = 'zahidulsifat@gmail.com';
 $headers = "From: $email_from \r\n";
 
 $headers .= "Reply-To: $visitor_email \r\n";
